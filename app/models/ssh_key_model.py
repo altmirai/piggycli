1c4d8cfd1dec6421f1c7b76603e0d9464c8bad35
Kyle Stewart <kyle.w.stewart@mac.com>
2021-05-21 21:59:19 -0400

    ssh key model tests

--- app/models/ssh_key_model.py
+++ app/models/ssh_key_model.py
@@ -3,41 +3,33 @@ import uuid
 
 class SSHKey:
 
-    def __init__(self, name, id, material, fingerprint):
-        self.name = name
+    def __init__(self, id, name, material, fingerprint):
         self.id = id
+        self.name = name
         self.material = material
-        self.fingerpring = fingerprint
+        self.fingerprint = fingerprint
 
     @classmethod
     def create(cls, client):
-
         name = f'Piggy_SSH_Key_{str(uuid.uuid4())[:8]}'
         resp = client.create_key_pair(KeyName=name)
-
         key = SSHKey(
-            name=resp['KeyName'],
             id=resp['KeyPairId'],
+            name=resp['KeyName'],
             material=resp['KeyMaterial'],
-            fingerprint=resp['KeyFingerprint']
-        )
-
+            fingerprint=resp['KeyFingerprint'])
         return key
 
     @classmethod
-    def all(cls, **kwargs):
-        client = kwargs['client']
-        key_pairs = client.describe_key_pairs()['KeyPairs']
+    def all(cls, client):
+        resp = client.describe_key_pairs()
+        key_pairs = resp['KeyPairs']
         for key in key_pairs:
             if key.get('KeyFingerprint') is not None:
                 del key['KeyFingerprint']
             if key.get('Tags') is not None:
                 del key['Tags']
         return key_pairs
-
-    def read(self):
-        resp = self.client.describe_key_pairs(KeyPairIds=[self.id])
-        return resp['KeyPairs'][0]
 
     def write_to_file(self, path):
         with open(f'{path}/{self.name}.pem', 'w') as file:
