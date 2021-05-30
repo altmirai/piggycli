c7b0090329ef82ef01e53a0fe44668bda5419de8
Kyle Stewart <kyle.w.stewart@mac.com>
2021-05-29 21:30:18 -0400

    more test

--- conftest.py
+++ conftest.py
@@ -25,6 +25,9 @@ class CredentialsData:
         self.KeyMaterial = '-----BEGIN RSA PRIVATE KEY-----\nMIIEpQIBAAKCAQEAkOrCB3e0Fj/Cv797THZn5YgxIPywNdlg284rMSshrLl8QC83\n0ck0K9CP3Y+rCuHGx7t/2tCtl66uKlwOPFvWGDi+akonkUeVqnV8U1z5jNhI8SwY\niXtcFX0twIGHaaxYQrWZvOUAnmE8JUGd7Pysy4Sy7/ZEldXwEN3fN2NIPRnQwii7\nS5tv573C2am2MMXtwEKtQi3uWgPu//maXqoM0/PuxTDk9DUKnN88nvNBMoTlHr1P\nl9QsHMPyXvJ/+TTPdVybXwMvv1KgCMeGid43CPu7SFa8trx9DuvSY03TwYyhZIp6\nMsPZot6pu+opRXgF7SkSpp/+ABPoA836sPF0+wIDAQABAoIBAAESi68Mdru3axSK\nMTpmoewz7tEkrZUob6wQwYcSn6QslzvOXaZiy80LNRVZq9VfyF3QCGkxJCe8NjPA\nDKbrsxDo0pfsxpAvrG7fgbUIOhyNuTR3tBLIY+0QyRbknoDsspaDy4h3VWLWq2BH\nNQj88bZr2/skomtNcwJc8frx9CXnmR1erB8d7UybKFiYL4ggM/MVQbdn66ZpmKCK\naZ774lbgdiwp8YZp1ANFw7zBr1MTqXKLmghtYZoornRUVk2c3OPUII62jmarUaqn\nKL+q2198j0axDsmFCAALTbmxjo//XWxwTeaRNwsi0hqeENEy1ywtidn1A8eHJaOi\nvGlV6LkCgYEA4rabAGB7OQTunGj6BiQBN8AnG/1B77HBA/VRy3YUHdPaBYtldLuX\nZrQKuE3l13uX0whXr+BQVDtDiegocz4r41/MR9uUoQFHnsL25D+A44xveT2scTHm\nBHpUprq8ti4XyNJGpnZXSxQCRzhcGnJItvfOaQWW+slEnR4RUuWCdjUCgYEAo6Mn\nmT+NyNCfJHWvF951fvRi7liktRzIcIL7Hu4gDkTTJ7U/Ms3OgQTsQ6VIfNeotAYC\nFe/GVb+SB91MnnCFwPbwt1vIwdLGvKtAUq3OQvE8Jv1LCC3XQJcRv/tJuP6lLz1t\nWNF59Uz0Ar6VN9tIctdribKGP6Dxg32OLlrV5G8CgYEAotnQlYC4gsjMLYYqsuaC\nCW35qd1N08O3hgRd8OysnpBi98Cd7DAkHR4O5TzvcM3SzUAc3LUgfqDjbthY1g8+\nr2FM+AD+zniA3cXmWyZSiyGBoXFvwQ+6zlShIfLZQ3PwmcyR+1jec4u35zjQ0B5v\npR50InRlc1fH9aR3hThfclECgYEAh+bv80GqIpbJJQGsKnmyQX78TxFFsbk26uKN\nZxHDg7Y7XCYWV74/fD23bzLtMen2DZVT1B4wLXUN9gQgJxIys6EjKFVNNVQ1g+oC\nYOhCfqxVFdiVoTRZKiaNMlGj18V9MO+mSfangEep/EGGMj6nO+GXSWQARQYIrvju\nxabhL3cCgYEAlPAbQgqljbfs6pIG5eEKHNLXaCZxFeZDQ+PQ9CYBsCL0nPo0e1iw\nHNFx2A/BN0hEZxiOO0LnDV5paFhAd+rFhSvxdgwhOBvxU1TDwcNCoyqpbJ57BB3e\n5d+ShkCu0K3LbKn65ZAtvJCB9yleu5VUIt6i+q/aaPLcWTcf+YxVDfM=\n-----END RSA PRIVATE KEY-----'
         self.KeyName = 'Piggy_SSH_Key_0194afd1'
         self.KeyPairId = 'key-0a070814c64fa0e65'
+        self.KeyFingerprint2 = 'de:7b:e3:aa:81:52:89:be:a5:01:a0:87:8d:67:76:47:e8:a5:9f:0c'
+        self.KeyName2 = 'Piggy_SSH_Key_40cc19f5'
+        self.KeyPairId2 = 'key-071d152e1ec5428df'
 
     @property
     def credentials_kwargs(self):
@@ -50,16 +53,48 @@ class CredentialsData:
             'KeyPairId': self.KeyPairId
         }
 
+    @property
+    def ssh_key_describe_resp(self):
+        return {
+            'KeyPairs': [
+                {
+                    'KeyPairId':  self.KeyPairId,
+                    'KeyFingerprint':  self.KeyFingerprint,
+                    'KeyName': self.KeyName,
+                    'Tags': []
+                },
+                {
+                    'KeyPairId': self.KeyPairId2,
+                    'KeyFingerprint': self.KeyFingerprint2,
+                    'KeyName': self.KeyName2,
+                    'Tags': []
+                }
+            ]}
+
+    @property
+    def ssh_key_kwargs(self):
+        return {
+            'id': self.KeyPairId,
+            'name': self.KeyName,
+            'material': self.KeyMaterial,
+            'fingerprint': self.KeyFingerprint
+        }
+
 
 t = CredentialsData()
 
 
 def pytest_sessionstart(session):
-    os.mkdir(t.test_path)
+    if os.path.isdir(t.test_path):
+        files = os.listdir(t.test_path)
+        for file in files:
+            os.remove(os.path.join(t.test_path, file))
+    else:
+        os.mkdir(t.test_path)
     set_env_var(var='PATH', value=t.test_path)
 
 
-@pytest.fixture
+@ pytest.fixture
 def credentials():
     credentials = Credentials.create(path=t.test_path, **t.credentials_kwargs)
     yield credentials
@@ -67,7 +102,7 @@ def credentials():
     os.rmdir(os.path.join(t.test_path, '.piggy'))
 
 
-@pytest.fixture
+@ pytest.fixture
 def ssh_key_ec2_test_create():
     client = botocore.session.get_session().create_client('ec2')
     with Stubber(client) as stubber:
@@ -79,37 +114,32 @@ def ssh_key_ec2_test_create():
         yield client
 
 
-@pytest.fixture
+@ pytest.fixture
 def ssh_key_ec2_test_all():
     client = botocore.session.get_session().create_client('ec2')
     with Stubber(client) as stubber:
         stubber.add_response(
             'describe_key_pairs',
-            {
-                'KeyPairs': [
-                    {
-                        'KeyPairId': 'key-0a070814c64fa0e65',
-                        'KeyFingerprint': '08:c9:28:e5:24:38:d5:ef:9b:a1:76:22:9f:00:0c:eb:47:16:59:cd',
-                        'KeyName': 'Piggy_SSH_Key_0194afd1',
-                        'Tags': []
-                    },
-                    {
-                        'KeyPairId': 'key-071d152e1ec5428df',
-                        'KeyFingerprint': 'de:7b:e3:aa:81:52:89:be:a5:01:a0:87:8d:67:76:47:e8:a5:9f:0c',
-                        'KeyName': 'Piggy_SSH_Key_40cc19f5',
-                        'Tags': []
-                    }
-                ]},
+            t.ssh_key_describe_resp,
             {}
         )
         yield client
 
 
 @pytest.fixture
+def ssh_key():
+    ssh_key = SSHKey(**t.ssh_key_kwargs)
+    yield ssh_key
+
+
+@ pytest.fixture
 def test_data():
     return CredentialsData()
 
 
 def pytest_sessionfinish(session, exitstatus):
+    files = os.listdir(t.test_path)
+    for file in files:
+        os.remove(os.path.join(t.test_path, file))
     os.rmdir(t.test_path)
     set_env_var(var='PATH', value=t.production_path)
--- tests/test_controllers/test_credentials_controller.py
+++ tests/test_controllers/test_credentials_controller.py
@@ -1,5 +1,4 @@
 from app.controllers.credentials_controller import CredentialsController
-from conftest import CredentialsData
 import os
 import json
 
@@ -27,3 +26,15 @@ def test_update(credentials, test_data):
     resp = json.loads(resp_json)
 
     assert resp['data']['crypto_user_password'] == 'password2'
+
+
+def test_write_to_file(ssh_key, test_data):
+    ssh_key_file = ssh_key.write_to_file(
+        cluster_id=test_data.cluster_id, path=test_data.test_path)
+
+    assert os.path.exists(ssh_key_file)
+
+    with open(ssh_key_file, 'r') as file:
+        ssh_key_material = file.read()
+
+    assert ssh_key_material == test_data.KeyMaterial
