57bbd91ce4b975318bf120c4f4576de39af89cd3
Kyle Stewart <kyle.w.stewart@mac.com>
2021-06-05 07:05:33 -0400

    tests

--- app/models/address_model.py
+++ app/models/address_model.py
@@ -1,16 +1,39 @@
-from app.models.s3_model import S3
 import hashlib
 from base58 import b58encode_check
 from ecdsa import VerifyingKey
 import json
 
 
-class Address(S3):
+class Address():
     def __init__(self, id, pub_key_pem, pub_key_handle, private_key_handle):
         self.id = id
         self.pub_key_pem = pub_key_pem
         self.pub_key_handle = pub_key_handle
         self.private_key_handle = private_key_handle
+
+    @classmethod
+    def all(cls, bucket, s3):
+        addresses = []
+        resp = s3.list_objects(Bucket=bucket)
+        keys = resp['Contents']
+        for key in keys:
+            address = cls.find(bucket=bucket, s3=s3, id=key['Key'])
+            addresses.append(address)
+        return addresses
+
+    @classmethod
+    def find(cls, bucket, s3, id):
+        resp = s3.get_object(Bucket=bucket, Key=id)
+        resp_data_json = resp['Body'].read().decode()
+        resp_data = json.loads(resp_data_json)
+        address = cls(
+            id=id,
+            pub_key_pem=resp_data['pub_key_pem'],
+            pub_key_handle=resp_data['pub_key_handle'],
+            private_key_handle=resp_data['private_key_handle']
+        )
+
+        return address
 
     @classmethod
     def create(cls, pub_key):
@@ -25,6 +48,31 @@ class Address(S3):
     def address(self):
         p2pkh = P2PKH(pem=self.pub_key_pem)
         return p2pkh.address
+
+    def save(self, bucket, s3, region):
+        if bucket_exists(bucket=bucket, s3=s3) is False:
+            create_bucket(bucket=bucket, s3=s3, region=region)
+
+        key = self.id
+        data_json = json.dumps(
+            {
+                'pub_key_handle': self.pub_key_handle,
+                'private_key_handle': self.private_key_handle,
+                'pub_key_pem': self.pub_key_pem,
+                'address': self.address
+            }
+        )
+        data_bytes = bytes(data_json, 'UTF-8')
+        resp = s3.put_object(
+            Body=data_bytes,
+            Bucket=bucket,
+            Key=key
+        )
+
+        assert resp['ResponseMetadata'][
+            'HTTPStatusCode'] == 200, f'Failed to save address: {self.id} to bucket: {bucket}'
+
+        return self.id
 
 
 class P2PKH:
@@ -51,3 +99,21 @@ class P2PKH:
         addr = b58encode_check(hashed_btc_public_key).decode('utf-8')
 
         return addr
+
+
+def bucket_exists(bucket, s3):
+    resp = s3.list_buckets()
+    for _bucket in resp['Buckets']:
+        if _bucket['Name'] == bucket:
+            return True
+    return False
+
+
+def create_bucket(bucket, s3, region):
+    location = {'LocationConstraint': region}
+    resp = s3.create_bucket(
+        Bucket=bucket,
+        CreateBucketConfiguration=location
+    )
+
+    return resp['Location']
--- tests/test_models/test_address_model.py
+++ tests/test_models/test_address_model.py
@@ -1,9 +1,71 @@
-from app.models.address_model import Address
-from tests.mocks import instance, cluster
+from app.models.address_model import Address, bucket_exists, create_bucket
 import tests.data as data
+
+from unittest.mock import patch
 
 
 def test_create(pub_key):
     address = Address.create(pub_key=pub_key)
+
+    assert address.address == data.address
+
+
+def test_bucket_exists(list_buckets_true):
+    stubber, client = list_buckets_true
+    with stubber:
+        resp = bucket_exists(bucket=data.bucket_name, s3=client)
+
+    assert resp == True
+
+
+def test_bucket_exists(list_buckets_false):
+    stubber, client = list_buckets_false
+    with stubber:
+        resp = bucket_exists(bucket=data.bucket_name, s3=client)
+
+    assert resp == False
+
+
+def test_create_bucket(aws_create_bucket):
+    stubber, client = aws_create_bucket
+    with stubber:
+        resp = create_bucket(
+            bucket=data.bucket_name,
+            s3=client,
+            region=data.aws_region
+        )
+
+    assert resp == data.create_bucket_resp['Location']
+
+
+def test_address_save(put_object, address):
+    stubber, mock_client = put_object
+    with stubber:
+        with patch('app.models.address_model.bucket_exists', return_value=True, autospec=True):
+            resp = address.save(
+                bucket=data.bucket_name,
+                s3=mock_client,
+                region=data.aws_region
+            )
+
+    assert resp == address.id
+
+
+def test_all(list_objects, address):
+    stubber, client = list_objects
+    with stubber:
+        with patch.object(Address, 'find', return_value=address):
+            addresses = Address.all(bucket=data.bucket_name, s3=client)
+
+    assert addresses[0].id == address.id
+
+
+def test_find(get_object):
+    stubber, client = get_object
+    with stubber:
+        address = Address.find(
+            bucket=data.bucket_name,
+            s3=client, id=data.address_id
+        )
 
     assert address.address == data.address
